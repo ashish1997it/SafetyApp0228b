c410ce39f90d90063f6af3a670862033b3d232bf
ashish1997it <sondagarashish@gmail.com>
2018-03-21 20:33:27 +0530

    UserDetailsForm-v69b

--- app/src/main/java/com/ashish/dev/safetyapp0228b/webView11.java
+++ app/src/main/java/com/ashish/dev/safetyapp0228b/webView11.java
@@ -2,14 +2,12 @@ package com.ashish.dev.safetyapp0228b;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
 
-public class form2 extends AppCompatActivity {
+public class webView11 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_form2);
+        setContentView(R.layout.activity_web_view11);
     }
 }
